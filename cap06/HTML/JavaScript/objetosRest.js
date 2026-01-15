bd1eff88cf458c13c1b4b5804be391ba57201641
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-01-15 18:18:54 -0300

    sobre Rest (resto)

---
+++ cap06/HTML/JavaScript/objetosRest.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+
+const pessoas = ["Ana", "Maria", "Guilherme", "Monteiro"];
+const resultado = [a, b, ...outros] = pessoas
+for ( valor of resultado) {
+    console.log(valor)
+}
+
+console.log(outros)
+
