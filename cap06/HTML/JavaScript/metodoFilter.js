40a1aa7661cc7719ad4f6c2775c44a24c4d0652b
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-01-17 17:00:44 -0300

    usando o metodo filter

---
+++ cap06/HTML/JavaScript/metodoFilter.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")();
+// usando o metodo filter()
+// assim como o metodo map, ele cria uma novo vetor,
+// so que com elementos de um condiçao, se caso for true ele armazena 
+// no no vetor
+const list =[2, 3, 5, 7, 8, 9, 12];
+const pares = list.filter(temp => temp % 2 == 1);
+
+console.log(pares)
+
+const aux = {nome: "Guilherme", idade: 20};
+
+console.log(aux.nome)
